e7f8ee53d4827d0e4fd51fd1a14ee1799c0c1be0
Long vo <longvt@minerva.vn>
2023-02-19 23:04:35 +0700

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,26 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { SettingsConsumer, SettingsProvider } from '@/contexts/settings-context';
+import {createTheme} from "@/theme";
+import { ThemeProvider } from '@mui/material';
+import {RTL} from "@/components/rtl";
+
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return  (
+    <SettingsProvider>
+    <SettingsConsumer>
+      {({ settings }) => (
+        <ThemeProvider theme={createTheme({
+          direction: settings.direction,
+          responsiveFontSizes: settings.responsiveFontSizes,
+          mode: settings.theme}
+        )}>
+            <RTL direction={settings.direction}>
+              <Component {...pageProps} />
+            </RTL>
+          </ThemeProvider>
+      )}
+    </SettingsConsumer>
+  </SettingsProvider>)
 }
